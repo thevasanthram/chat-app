2810e42a29a6036d19327f4fbd2a0c24d64553f2
thevasanthram <thevasanthram@github.com>
2022-05-25 22:55:54 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+
+const app = express()
+
+
+app.get('/' ,(req, res)=>{
+    res.send('hello world')
+})
+app.listen(80, () => {
+    console.log('server listening on port 80')
+})
